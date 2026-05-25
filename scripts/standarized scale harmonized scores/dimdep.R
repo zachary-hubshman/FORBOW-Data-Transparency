568df70e433ce96f0f9414070f8a0c562e83649d
zc390390 <zc390390@dal.ca>
2026-05-25 14:35:03 -0300

    Updated dimdep wording

--- scripts/standarized scale harmonized scores/dimdep.R
+++ scripts/standarized scale harmonized scores/dimdep.R
@@ -4,8 +4,8 @@
 FOR <- FOR %>%
   mutate(
 
-    MFQ_C_z = as.numeric(scale(mfqytot)),
-    MFQ_P_z = as.numeric(scale(mfqptot)),
+    MFQ_C_z = as.numeric(scale(mfq_y_tot)),
+    MFQ_P_z = as.numeric(scale(mfq_p_tot)),
 
     dimdep   = rowMeans(across(c(MFQ_C_z, MFQ_P_z)), na.rm = TRUE)
   )
